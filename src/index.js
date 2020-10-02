ba96ea801a1ae8ebdc7891e9e37aa4cdbb73fa15
war-art <kamronmirsaidov00@gmail.com>
2020-10-02 18:11:53 +0500

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  return ((typeof array == 'undefined' || array.length == 0 ? 0 : Math.min.apply(null, array)));
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+  return ((typeof array == 'undefined' || array.length == 0 ? 0 : Math.max.apply(null, array)));
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (typeof array == 'undefined' || array.length == 0) {
+      return 0;
+
+  } else {
+      let i = 0,
+          sum = 0,
+          len = array.length;
+      while (i < len) {
+          sum = sum + array[i++];
+
+      }
+      return sum / len;
+  }
 }
